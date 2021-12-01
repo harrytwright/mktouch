b7d0ee5511db65b5c49868f8949e03147ebefe24
harrytwright <haroldtomwright@gmail.com>
2021-12-01 15:43:14 +0000

    initial commit

---
+++ Readme.md
@@ -0,0 +1,31 @@
+# mktouch(1)
+
+This is a utility to allow for creating and populating folders
+
+> This is a little wrapper for `mkdir` & `touch`
+
+## Installing
+
+```shell
+$ curl https://raw.githubusercontent.com/harrytwright/mktouch/main/mktouch.sh -o mktouch.sh
+
+$ cp ./mktouch.sh /usr/local/bin/mktouch
+```
+
+
+## Usage
+
+Creating 4 temp dirs and adding an index.sh file inside each
+
+```shell
+# command       <dirs>          - <files>
+$ ./mktouch.sh ./temp/{1,2,3,4} - index.sh
+```
+
+An actual use case, creating the boilerplate for a React component
+
+```shell
+$ ./mktouch.sh ./src/components/Input - {index,Wrapper,hooks,reducer,actions}.js
+
+$ ./mktouch.sh ./src/plugins/PluginName - {plugin,installer,builder}.rb
+```
---
+++ mktouch.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# By: Harry Wright <haroldtomwright@gmail.com>
+# License: Apache License 2.0
+# Source repo: https://github.com/harrytwright/mktouch
+
+set -e
+# Help options
+opt_h=0
+# Get the commands
+MKDIR=$(which mkdir)
+TOUCH=$(which touch)
+# Reset
+RESET=$(tput sgr0)    # Text Reset
+# Regular Colors
+RED=$(tput setaf 1)          # Red
+
+usage() {
+  echo "$(basename $0) [opts] <directory> - <files>"
+  echo ""
+  echo "Example:"
+  echo "  $ $(basename $0) ./src/example -- {index,demo}.js"
+  echo ""
+  echo "Options:"
+  echo "  -h:        This help message"
+  echo ""
+  [ "$opt_h" = "1" ] && exit 0 || exit 1
+}
+
+create_path () {
+  arguments=("$@")
+
+  dir=${arguments[0]}
+  unset 'arguments[0]'
+
+  echo "=> Creating ${dir}"
+  MKDIR -p "${dir}"
+
+  for file in "${arguments[@]}" ; do
+      TOUCH "${dir}/${file}"
+  done
+}
+
+while getopts 'h:' opt; do
+  # shellcheck disable=SC2220
+  case $opt in
+    h) opt_h=1;;
+  esac
+done
+shift $(expr "$OPTIND" - 1)
+
+if [ $# -le 1 -o "$opt_h" = "1" ]; then
+  usage
+fi
+
+DIRS=()
+FILES=()
+
+# Get all the dirs
+for argument in "$@"; do
+  if [ "${argument}" == '-' ]; then
+    shift;
+    break;
+  fi
+
+  DIRS+=("${argument}")
+  shift;
+done
+
+if [ ${#DIRS[@]} -lt 1 ]; then
+  echo "${RED}No directories to create${RESET}"
+  echo ""
+  usage
+fi
+
+# Get the files
+for argument in "$@"; do
+  FILES+=("${argument}")
+done
+
+if [ ${#FILES[@]} -lt 1 ]; then
+  echo "${RED}No files to create${RESET}"
+  echo ""
+  usage
+fi
+
+# Run the script
+for directory in "${DIRS[@]}" ; do
+    create_path "${directory}" "${FILES[@]}"
+done
